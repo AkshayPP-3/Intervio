8c89aa24e6a4593692fa16e44d22a7d61cdfb5b7
AkshayPP-3 <24061100617@reva.edu.in>
2026-08-31 17:50:05 +0530

    used imported packages such as helmet , morgan , etc

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,24 @@ import cors from "cors";
 import morgan from "morgan";
 import helmet from "helmet";
 
-const app = express();
+dotenv.config();
 
+const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  }),
+);
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  }),
+);
+app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`);
+});
