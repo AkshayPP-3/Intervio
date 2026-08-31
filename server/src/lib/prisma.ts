b8a28ee9855ae81343b1a1f066cbc0dfb2c955ce
AkshayPP-3 <24061100617@reva.edu.in>
2026-08-31 18:05:08 +0530

    edited prisma adapter

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,12 +1,9 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
-import pg from "pg";
+import { PrismaClient } from "../../generated/prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const pool = new pg.Pool({ connectionString });
-const adapter = new PrismaPg(pool);
+const connectionString = process.env.DATABASE_URL as string;
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
