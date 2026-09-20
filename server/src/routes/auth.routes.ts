a8564eb8237529fc1428214add259bf4bba4b9b3
AkshayPP-3 <24061100617@reva.edu.in>
2026-09-21 01:02:12 +0530

    POST- created router for register

---
+++ server/src/routes/auth.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import validate from "../middleware/validate.middleware";
+import authController from "../controllers/auth.controller";
+import { registerSchema, loginSchema } from "../schemas/auth.schema";
+import { register } from "node:module";
+
+const router = Router();
+
+router.post(
+    "/register",
+    validate(registerSchema),
+    authController.register,
+)
