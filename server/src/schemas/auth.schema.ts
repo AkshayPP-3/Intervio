767ced50e115b305d159e1345d10df38c7338d12
AkshayPP-3 <24061100617@reva.edu.in>
2026-09-20 23:05:30 +0530

    exported login schema and completed auth schema

--- server/src/schemas/auth.schema.ts
+++ server/src/schemas/auth.schema.ts
@@ -16,3 +16,14 @@ export const registerSchema = z.object({
         .min(8,"Password must be at least 8 characters")
         .max(100,"Password must not exceed 100 characters"),
 });
+
+export const loginSchema = z.object({
+    email: z
+        .string()
+        .trim()
+        .email("Please provide a valid email address")
+        .toLowerCase(),
+    password: z
+        .string()
+        .min(1,"Password is required"),
+})
