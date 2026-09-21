aa8ac3279a95bb1badeef9b3eb14492d41455a37
AkshayPP-3 <24061100617@reva.edu.in>
2026-09-21 09:33:53 +0530

    update user

--- server/src/services/user/user.service.ts
+++ server/src/services/user/user.service.ts
@@ -1,5 +1,7 @@
+import { email, string } from "zod";
 import {prisma} from "../../config/prisma";
 import appError from "../../utils/appError";
+import { throwDeprecation } from "node:process";
 
 const getUserById = async (userId: string)=>{
     const user = await prisma.user.findUnique({
@@ -18,4 +20,32 @@ const getUserById = async (userId: string)=>{
         throw new appError("User not found",404);
     }
     return user;
+}
+
+const updateUser = async(
+    userId: string,
+    data: {
+        name?: string,
+        email?: string,
+    }
+)=>{
+    const user = await prisma.user.findUnique({
+        where:{
+            id: userId
+        }
+    })
+    if(!user){
+        throw new appError("User not found",404);
+    }
+    if(data.email && data.email!==user.email){
+        const existingUser = await prisma.user.findUnique({
+            where:{
+                email: data.email,
+            }
+        })
+        if(existingUser){
+            throw new appError("User with this email already exists",409);
+        }
+    }
+    
 }
