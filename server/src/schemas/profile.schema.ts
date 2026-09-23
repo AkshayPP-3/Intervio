03e347635e8b13d3b1ac9c3e464f42182fc1162c
AkshayPP-3 <24061100617@reva.edu.in>
2026-09-23 21:47:15 +0530

    exported create profile schema

---
+++ server/src/schemas/profile.schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const createProfileSchema = z.object({
+    bio: z.string().optional(),
+    phone: z.string().optional(),
+    location: z.string().optional(),
+    education: z.string().optional(),
+    skills: z.string().optional(),
+})
