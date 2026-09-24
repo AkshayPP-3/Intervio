71f5259fb47a432dd0cb5b5fd2578a03b42eb414
AkshayPP-3 <24061100617@reva.edu.in>
2026-09-24 23:54:58 +0530

    authentication check

--- server/src/controllers/profile.controller.ts
+++ server/src/controllers/profile.controller.ts
@@ -1,3 +1,15 @@
 import type { Request, Response, NextFunction } from "express";
 import profileServices from "../services/profile.services.js";
 
+const createProfile = async (req: Request, res: Response, next: NextFunction)=>{
+    try{
+        const userId = req.user?.userId;
+        if(!userId){
+            return res.status(401).json({
+                success: false,
+                message: "Authentication required",            })
+        }
+
+    
+    }
+}
